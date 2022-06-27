3801fe3696d03a4f3c24779d1d58bd0a277e3171
nik-trifonov <102224540+nik-trifonov@users.noreply.github.com>
2022-06-27 21:00:08 +0300

    Add files via upload

---
+++ Calculator/src/Calculator/jtxtResult.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calculator;
+
+/**
+ *
+ * @author ASUS
+ */
+class jtxtResult {
+
+    static void setText(String Nums) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
